63634c6f4c883ff65017790c9b96b47824f55263
Jonathan Olson <jonathanholson@gmail.com>
2025-11-30 19:43:25 -0800

    Initial working socket server; echoes message back to sender

--- src/main.c
+++ src/main.c
@@ -1,1 +1,113 @@
-int main() { return 0; }
+#include <arpa/inet.h>
+#include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 18000
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+int main() {
+    int listen_fd, new_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+
+    // Create listening socket
+    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    // Reuse address/port
+    int opt = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    // Bind socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
+        0) {
+        perror("bind");
+        close(listen_fd);
+        exit(1);
+    }
+
+    // Listen
+    if (listen(listen_fd, 5) < 0) {
+        perror("listen");
+        close(listen_fd);
+        exit(1);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    // Prepare poll fds
+    struct pollfd fds[MAX_CLIENTS + 1];
+    fds[0].fd = listen_fd;
+    fds[0].events = POLLIN; // Ready to accept connections
+
+    // Initialize all of the client fds to -1
+    for (int i = 1; i <= MAX_CLIENTS; i++) {
+        fds[i].fd = -1;
+    }
+
+    // Main loop
+    while (1) {
+        int activity = poll(fds, MAX_CLIENTS + 1, -1);
+        if (activity < 0) {
+            perror("poll");
+            break;
+        }
+
+        // New connection
+        if (fds[0].revents & POLLIN) {
+            new_fd =
+                accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
+            if (new_fd < 0) {
+                perror("accept");
+                continue;
+            }
+
+            printf("New client connected: %d\n", new_fd);
+
+            // Add to poll list
+            for (int i = 1; i <= MAX_CLIENTS; i++) {
+                if (fds[i].fd < 0) {
+                    fds[i].fd = new_fd;
+                    fds[i].events = POLLIN;
+                    break;
+                }
+            }
+        }
+
+        // Handle data from clients
+        for (int i = 1; i <= MAX_CLIENTS; i++) {
+            int fd = fds[i].fd;
+            if (fd < 0)
+                continue;
+
+            if (fds[i].revents & POLLIN) {
+                int bytes = recv(fd, buffer, sizeof(buffer) - 1, 0);
+                if (bytes < 0) {
+                    // Client disconnected
+                    printf("Client %d disconnected", fd);
+                    close(fd);
+                    fds[i].fd = -1;
+                } else {
+                    buffer[bytes] = '\0';
+                    printf("Received from %d: %s", fd, buffer);
+                    send(fd, buffer, bytes, 0); // Echo back
+                }
+            }
+        }
+    }
+
+    close(listen_fd);
+    return 0;
+}
